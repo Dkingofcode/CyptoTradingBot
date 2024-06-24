f94342414e0106093f2245b5f5c2561129874592
David <doladepo128@gmail.com>
2024-06-23 22:44:55 -0700

    getting a random symbol from phe_symbols.csv

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import dontshare_config as ds
 from dateline import date, datetime, timezone, tzinfo
 import time, schedule
 import nice_funcs as n
-
+import random
 
 phenex = ccxt.phenex({
     'enableRateLimit': True,
@@ -36,6 +36,9 @@ params = {'type': 'swap', 'code': 'USD'}
 # fetch all the market to get all the symbols
 markets = phenex.fetch_markets(params=params)
 temp_df = pd.DataFrame()
+symbols_df = pd.DataFrame()
+
+###### GETTING ALL THE SYMBOLS ######
 
 for n in markets:
     type = n['info']['type']
@@ -43,7 +46,7 @@ for n in markets:
         print(type)
         symbol = n['id']
         print(n['id'])
-        symbols_df['symbol'] = [symbol]
+        temp_df['symbol'] = [symbol]
 
         # make a df to store
     
@@ -52,7 +55,19 @@ for n in markets:
 
     symbols_df = symbols_df.append(temp_df)
 
-print(symbols_df)    
+print(symbols_df)  
+symbols_df.to_csv('phe_symbols.csv', index=False)  
+
+all_symbols = pd.read_csv('phe_symbols.csv')
+print(all_symbols[5:35])
+
+random_symbol_num = random.randrange(0, 78)
+random_symbol = all_symbols.iloc[random_symbol_num]['symbol']
+print(random_symbol)
+
+
+
+
 
 # outfile = open('jsontickers.json', 'w')
 # json.dump()
